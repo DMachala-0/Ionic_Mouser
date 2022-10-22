4a27ecf8210c4a20332b35ccc2da98bb31db1cda
DM <xxx@cc.cc>
2022-10-22 13:17:17 +0200

    PWA init

--- PWA_test/src/environments/environment.prod.ts
+++ PWA_test/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  api:{
+    key:"9eba3d3c-1025-43eb-bfa3-77eb48c26cf2",
+    baseUrl: 'https://api.mouser.com',
+    version: '1',
+    apiUrl: '/api/v',
+    searchUrl:  '/search/keyword',
+    keyUrl: '?apikey='
+  }
 };
